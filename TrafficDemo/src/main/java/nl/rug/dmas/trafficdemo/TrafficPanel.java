9c374b984cdf0653ca333bf4f73538758f0c18c4
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-09-23 21:52:45 +0200

    Render prettier

--- TrafficDemo/src/main/java/nl/rug/dmas/trafficdemo/TrafficPanel.java
+++ TrafficDemo/src/main/java/nl/rug/dmas/trafficdemo/TrafficPanel.java
@@ -11,6 +11,7 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import javax.swing.JPanel;
 import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.collision.shapes.PolygonShape;
@@ -71,7 +72,9 @@ public class TrafficPanel extends JPanel {
         paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
+        
         // This paint method gets called indirectly every 1/60th of a second
         // by the mainLoop which issues a 'repaint()' request. (AWT then
         // decides on when to do the actual painting, and at that moment this
