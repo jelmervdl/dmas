7c485ab4e5c517009444a3f262158f79096b3827
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-09-24 00:54:18 +0200

    Add more JavaDoc

--- TrafficDemo/src/main/java/nl/rug/dmas/trafficdemo/Driver.java
+++ TrafficDemo/src/main/java/nl/rug/dmas/trafficdemo/Driver.java
@@ -49,6 +49,9 @@ class Driver {
         return shape;
     }
     
+    /**
+     * Update the steering direction of the car we drive
+     */
     public void step()
     {
        Vec2 direction = new Vec2();
@@ -60,10 +63,15 @@ class Driver {
        setSteerDirection(direction);
     }
     
+    /**
+     * Set the speed and steering direction. This implementation right now uses
+     * a vector with a length and an angle to determine the speed and direction.
+     * @param direction vector from the current location to the goal location
+     */
     private void setSteerDirection(Vec2 direction) {
         car.setSteeringDirection(direction.mul(-1f));
         
-        car.setSpeedKMH(direction.length() * 10);
+        car.setSpeedKMH(direction.length() * 5);
 
         if (direction.length() > 0.5f)
             car.acceleration = Acceleration.ACCELERATE;
@@ -71,24 +79,41 @@ class Driver {
             car.acceleration = Acceleration.NONE;
     }
     
+    /**
+     * Behavior that determines the optimal direction to avoid other cars
+     * @return a vector direction
+     */
     private Vec2 steerToAvoidCars() {
         Vec2 direction = new Vec2(0, 0);
         
         for (Car other : getCarsInSight()) {
             Vec2 directionTowardsCar = car.body.getLocalPoint(other.body.getWorldCenter());
             
-            if (directionTowardsCar.length() < 5f)
-                direction.addLocal(directionTowardsCar.mul(-1));
+            if (directionTowardsCar.length() < 5f) {
+                // todo: Right now, cars that are far away have more impact on
+                // the direction sum than cars close by. Maybe that should be
+                // turned around.
+                direction.addLocal(directionTowardsCar.negate());
+            }
         }
         
         return direction;
     }
     
+    /**
+     * Behavior that determines the direction towards the mouse. The mouse
+     * location is written to the scenario's common knowledge table every update
+     * @return a vector direction (with length!)
+     */
     private Vec2 steerTowardsMouse() {
         Vec2 worldMouse = (Vec2) scenario.commonKnowledge.get("mouse");
         return car.body.getLocalPoint(worldMouse);
     }
     
+    /**
+     * Filters the list of fixtures in sight and only returns actual cars.
+     * @return a list of cars inside the FOV.
+     */
     private List<Car> getCarsInSight() {
         ArrayList<Car> cars = new ArrayList<>();
         for (Fixture fixture : fixturesInSight)
@@ -98,6 +123,11 @@ class Driver {
         return cars;
     }
     
+    /**
+     * Utility function that returns true if this cars sees other cars. Used by
+     * TrafficPanel to change the color of the FOV (if turned on).
+     * @return whether this car has other cars in its FOV.
+     */
     public boolean seesOtherCars() {
         return !getCarsInSight().isEmpty();
     }
