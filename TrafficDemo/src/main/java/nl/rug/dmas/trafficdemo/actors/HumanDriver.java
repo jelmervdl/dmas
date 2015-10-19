82c93e866eb0b1b2dd12a1dd7dc76e138d573389
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2015-10-19 19:57:15 +0200

    Move common behaviour to Driver superclass

--- TrafficDemo/src/main/java/nl/rug/dmas/trafficdemo/actors/HumanDriver.java
+++ TrafficDemo/src/main/java/nl/rug/dmas/trafficdemo/actors/HumanDriver.java
@@ -7,7 +7,6 @@ package nl.rug.dmas.trafficdemo.actors;
 
 import java.util.List;
 import nl.rug.dmas.trafficdemo.Acceleration;
-import nl.rug.dmas.trafficdemo.Car;
 import nl.rug.dmas.trafficdemo.Scenario;
 import nl.rug.dmas.trafficdemo.ShapeUtil;
 import nl.rug.dmas.trafficdemo.VecUtils;
@@ -63,9 +62,10 @@ public class HumanDriver extends Driver{
     {
         debugDraw.clear();
         
-        car.setSteeringDirection(steerTowardsPath().negate());
-       
         if (steerTowardsPath().length() == 0)
+        float steeringAngle = VecUtils.getAngle(steerTowardsPath().negate());
+        
+        car.setSteeringDirection(steeringAngle);
             car.setAcceleration(Acceleration.NONE);
         else if (speedAdjustmentToAvoidCars() < 0)
             car.setAcceleration(Acceleration.BRAKE);
@@ -75,56 +75,6 @@ public class HumanDriver extends Driver{
             car.setAcceleration(Acceleration.NONE);
     }
     
-    /**
-     * Behaviour that determines the optimal direction to avoid other cars
-     * @return a vector direction
-     */
-    private Vec2 steerToAvoidCars() {
-        Vec2 direction = new Vec2(0, 0);
-        
-        return direction;
-    }
-    
-    private float speedAdjustmentToAvoidCars() {
-        VecUtils.Intersection mostImportant = null;
-        
-//        debugDraw.draw(car.getPosition(), car.getAbsoluteVelocity());
-            
-        for (Car other : getCarsInSight()) {
-            // If that car is driving towards me, well, shit!
-            
-            debugDraw.draw(other.getPosition(), other.getAbsoluteVelocity());
-            
-            VecUtils.Intersection intersection = VecUtils.intersect(car.getPosition(), car.getAbsoluteVelocity(),
-                other.getPosition(), other.getAbsoluteVelocity());
-            
-            if (intersection == null)
-                continue;
-            
-            debugDraw.draw(intersection.position);
-            
-            if (mostImportant == null || Math.abs(mostImportant.u - mostImportant.v) > Math.abs(intersection.u - intersection.v))
-                mostImportant = intersection;
-        }
-        
-        if (mostImportant == null)
-            return 0;
-        
-        // Time until I reach the intersection minus the time the other
-        // reaches the intersection
-        float d = mostImportant.v - mostImportant.u;
-
-        // If there is enough time to pass safely, ignore this car
-        // Todo: determine 3 using the length of our and the other car and
-        // their speed. Hard math ahead?
-        //if (Math.abs(d) > 4)
-        //    return 0;
-        
-        // d < 0: I'm there first, we should speed up a bit maybe?
-        // d > 0: other is there first, we should brake?
-        return d;
-    }
-
     /**
      * @return the viewLength
      */
